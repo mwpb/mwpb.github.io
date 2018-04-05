2821d5bb09363c47ff5e555ff9c83a340d87e2e6
Matthew Burke <matthewpatrickburke@gmail.com>
2018-04-05 09:30:06 -0600

    added talk

--- elisp/generate-site.el
+++ elisp/generate-site.el
@@ -1,7 +1,6 @@
 (load-file "f.el")
 (load-file "layout.el")
 (load-file "generate-html.el")
-(load-file "generate-site.el")
 
 (f-write (page-from-body (talks-dl)) 'utf-8 "../talks.html")
 (f-write (page-from-body (papers-dl)) 'utf-8 "../papers.html")
