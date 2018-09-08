d7ec81a461b3a95bc45c3347e1a0f3c778d7d6da
Matthew Burke <matthewpatrickburke@gmail.com>
2018-09-08 13:51:15 -0600

    updated talks

--- curriculum-vitae/auto/matthew-burke-cv.el
+++ curriculum-vitae/auto/matthew-burke-cv.el
@@ -5,10 +5,10 @@
                      '(("article" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "margin=3cm")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "article"
