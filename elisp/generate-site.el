bdbd2cd391bb5ede1b3c79c07f1b0fc406d32707
Matthew Burke <matthewpatrickburke@gmail.com>
2018-10-20 11:17:00 -0600

    reefactored using jinja2

--- elisp/generate-site.el
+++
@@ -1,6 +0,0 @@
-(load-file "f.el")
-(load-file "layout.el")
-(load-file "generate-html.el")
-
-(f-write (page-from-body (talks-dl)) 'utf-8 "../talks.html")
-(f-write (page-from-body (papers-dl)) 'utf-8 "../papers.html")
